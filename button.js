8693c8b1f1655ece2db20d13eeb534ffc4d19ff7
Naira Kazeian <nkazeian@gmail.com>
2021-08-18 08:18:48 +0300

    cleared extra comments

--- button.js
+++ button.js
@@ -32,12 +32,7 @@ function runner() {
     console.log('run');
     const getColorId = randomIntegerFromInterval(0, colors.length);
     refs.body.style.background = colors[getColorId];
-   
-    // return refs.body.setAttribute('color', colors[getColorId]); - 2
-    // refs.body.classList.add("color"); -1
-    // console.log(a); -1
-    // refs.body.color = colors[getColorId]; -1
-}
+  }
 let id = null;
 function startBtnClick() {
    id=setInterval(runner, 1000);
@@ -49,35 +44,3 @@ function stopBtnClick() {
     refs.startBtn.disabled = false;
     clearInterval(id);
 }
-
-
-
-
-
-// const DELAY = 1000;
-// const intervalId = setInterval(() => {
-//     refs.body.classList.add('color');
-//     refs.body.color = colors[getColorId];
-// }, DELAY);
-
-
-
-//fix date of start
-//calculate delta of time
-//use setInterval
-
-// function logger() {
-//     console.log('run');
-// };
-// // let id = null;
-// function timerStart() {
-//     id=setInterval(logger, 1000);
-    // setInterval(logger, 1000);
-// };
-
-// put start.addEventListeners('click', timerStart)
-
-// function stopTimer() {
-//     clearInterval(id);
-// }
-//put stop.addEventListeners('click', stopTimer)
