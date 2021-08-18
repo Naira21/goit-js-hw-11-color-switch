5e615f05e7f1d5eb5e0f872e60631e1be27988c1
Naira Kazeian <nkazeian@gmail.com>
2021-08-18 08:07:15 +0300

    added color shifting

--- button.js
+++ button.js
@@ -1,3 +1,4 @@
+// Исходные данные
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -6,6 +7,12 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+//Реализация задачи
 
 
 const refs = {
@@ -19,13 +26,23 @@ refs.startBtn.addEventListener('click', startBtnClick);
 refs.stopBtn.addEventListener('click', stopBtnClick);
 refs.startBtn.disabled = false;
 
+
+
 function runner() {
     console.log('run');
+    const getColorId = randomIntegerFromInterval(0, colors.length);
+    refs.body.style.background = colors[getColorId];
+   
+    // return refs.body.setAttribute('color', colors[getColorId]); - 2
+    // refs.body.classList.add("color"); -1
+    // console.log(a); -1
+    // refs.body.color = colors[getColorId]; -1
 }
 let id = null;
 function startBtnClick() {
    id=setInterval(runner, 1000);
-    refs.startBtn.disabled = true;    
+    refs.startBtn.disabled = true;
+    
 }
 
 function stopBtnClick() {
@@ -33,19 +50,15 @@ function stopBtnClick() {
     clearInterval(id);
 }
 
-// Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval.
 
- const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
 
-const getColorId = randomIntegerFromInterval(0, colors.length);
 
-const DELAY = 1000;
-const intervalId = setInterval(() => {
-    refs.body.classList.add('color');
-    refs.body.color = colors[getColorId];
-}, DELAY);
+
+// const DELAY = 1000;
+// const intervalId = setInterval(() => {
+//     refs.body.classList.add('color');
+//     refs.body.color = colors[getColorId];
+// }, DELAY);
 
 
 
